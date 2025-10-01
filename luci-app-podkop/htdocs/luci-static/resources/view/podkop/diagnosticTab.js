7118d2a49947e1aa29d6fc11ccf02e1dc0c72c22
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2025-10-01 16:17:28 +0300

    refactor: remove unused command show_luci_version, show version from LuCI call

--- luci-app-podkop/htdocs/luci-static/resources/view/podkop/diagnosticTab.js
+++ luci-app-podkop/htdocs/luci-static/resources/view/podkop/diagnosticTab.js
@@ -322,7 +322,7 @@ function showConfigModal(command, title) {
         let formattedOutput = '';
 
         if (command === 'global_check') {
-            safeExec('/usr/bin/podkop', [command], 'P0_PRIORITY', res => {
+            safeExec('/usr/bin/podkop', [command, constants.PODKOP_LUCI_APP_VERSION], 'P0_PRIORITY', res => {
                 formattedOutput = formatDiagnosticOutput(res.stdout || _('No output'));
 
                 try {
